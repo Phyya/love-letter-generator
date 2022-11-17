7748269ffc42f996730a94223ad8bf49bf40db5b
Wisdom Ochiche (Juicio) <wisdomochiche@gmail.com>
2022-11-17 21:08:28 +0100

    eslint trial

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 function App() {
   return (
     <div className="w-screen h-screen flex flex-col font-avenir">
-      Hello Team Engine
+      Hello, hey Team Engine
     </div>
   );
 }
