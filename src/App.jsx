710049d7fc066cd896d8b4063ed6d53dbda969b6
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-11-17 23:16:42 +0100

    created the button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="w-screen h-screen flex flex-col font-avenir">
       Hello Team Engine
+      <div style={{ width: '100px', height: '40px' }}>
+        <Button />
+      </div>
     </div>
   );
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ButtonWrapper } from './style';
+
+const Button = () => {
+  return <ButtonWrapper disabled={true}>Button</ButtonWrapper>;
+};
+
+export default Button;
