595f4d84bba41062b45580ec65bc4f6dfa091243
Osebest <udazi.osebest@gmail.com>
2022-11-18 19:01:43 +0100

    Finished with the Terms of Service page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import TermsOfService from './pages/TermsOfService';
 
 function App() {
   return (
-    <div className="w-screen h-screen flex flex-col font-avenir">
-      Hello Team Engine
+    <div className="w-screen h-screen flex flex-col font-avenir mt-5 p-5">
+      <TermsOfService />
     </div>
   );
 }
