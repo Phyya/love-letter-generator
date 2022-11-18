41a79653c3f03a9328f2a03a7670bed9eeeceb13
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-11-18 13:43:47 +0100

    input field and button implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Button from './components/Button/Button';
 
 function App() {
   return (
     <div className="w-screen h-screen flex flex-col font-avenir">
       Hello Team Engine
-      <Button width="100px" height="40px" />
     </div>
   );
 }
