836b2188506106cc95a12520148c839d2c88b940
Osebest <udazi.osebest@gmail.com>
2022-11-19 09:46:28 +0100

    Made router changes for merging

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import TermsOfService from './pages/TermsOfService';
+import { Routes, Route } from 'react-router';
 
 function App() {
   return (
-    <div className="w-screen h-screen flex flex-col font-avenir mt-5 p-5">
-      <TermsOfService />
-    </div>
+    <>
+      <Routes>
+        <Route exact path="/termsofservice" element={<TermsOfService />} />
+      </Routes>
+    </>
   );
 }
 
