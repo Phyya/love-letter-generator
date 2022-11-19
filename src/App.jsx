b2b132e3285d284260bf3895d24d86f2fb6b5220
nwosu chimezie <nwosuchimezie95@gmail.com>
2022-11-19 13:36:29 +0000

    completed about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
     <div className="w-screen h-screen flex flex-col font-avenir">
-      Hello Team Engine
+      <AboutPage />
     </div>
   );
 }
