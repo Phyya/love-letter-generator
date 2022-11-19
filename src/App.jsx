29d5218d03af2f1114c49722fcb2a5fa9b04257d
Ugbah Isioma <ugbahisioma@gmail.com>
2022-11-19 15:23:03 +0100

    feat: added profile page and respective components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
   return (
     <div className="w-screen h-screen flex flex-col font-avenir">
-      Hello Team Engine
+      <ProfilePage />
     </div>
   );
 }
