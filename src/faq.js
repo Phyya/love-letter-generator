e1c185d6bac6f62cdbb67208fefa29ce859ea36c
ScottDeev <amehelijah99@gmail.com>
2022-11-20 18:16:11 +0100

    fix: style issues on faq

--- src/faq.js
+++ src/faq.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import React from 'react';
 //import {BsCaretDown} from 'react-icons/bs'
-
+import arrowdown from './assets/arrowdown.svg'
 import './faq.css';
 export function Faq(props) {
   const obj = props;
@@ -15,11 +15,11 @@ export function Faq(props) {
 
   return (
     <section key={id} className="faq">
-      <div className="title-button-container">
+      <div className="title-button-container text-[16px] flex justify-between w-full">
         <h2>{title}</h2>
-        <button onClick={showInfoBtn}>{showInfo ? '-' : '+'}</button>
+        <img className={`${showInfo ? 'rotate-180':''}`} src={arrowdown} onClick={showInfoBtn}/>
       </div>
-      {showInfo && <p>{info}</p>}
+      {showInfo && <p className='text-[18px] px-[10px] py-[15px]'>{info}</p>}
     </section>
   );
 }
