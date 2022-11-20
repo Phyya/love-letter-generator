f20fd7d988475f87a49974d613b01c1e9169505c
Raga <ragawilliam570@gmail.com>
2022-11-20 20:57:14 +0300

    Fix privacy control and career page build

--- src/pages/CareerScreens/CareerPage.jsx
+++ src/pages/CareerScreens/CareerPage.jsx
@@ -19,70 +19,70 @@ const CareerPage = () => {
   };
 
   return (
-  <>
-    <Navbar />
-    <div className="max-w-6xl m-auto p-5">
-      <section className="relative w-full flex flex-row gap-5 justify-between">
-        <input
-          type="text"
-          className="border border[#d0d5dd] outline-none h-[50px] w-4/5 pl-8 rounded-lg"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          placeholder="Search positions"
-        />
-        <AiOutlineSearch className="absolute top-7 left-2 w-7 h-7 text-[#b4b9bf]" />
-        <button
-          type="submit"
-          className="bg-main-1 border-none text-white flex justify-center items-center px-10 rounded-lg"
-        >
-          Search
-        </button>
-      </section>
-      <section className="mt-7 mb-10 flex gap-5 text-3xl">
-        <p onClick={() => setData(CareerPageData)} className="cursor-pointer">
-          All
-        </p>
-        <p
-          onClick={() => filterResult('Lead Generation')}
-          className="cursor-pointer"
-        >
-          Lead Generation
-        </p>
-        <p onClick={() => filterResult('Others')} className="cursor-pointer">
-          Others
-        </p>
-      </section>
-
-      {data
-        .filter((item) => {
-          return search.toLowerCase() === ''
-            ? item
-            : item.Role.toLowerCase().includes(search);
-        })
-        .map((item) => {
-          return (
-            <div key={item.sn}>
-              <h2 className="text-3xl">{item.Category}</h2>
-              <div className="bg-[#f0f0f0] flex justify-between items-center p-8 mt-8 mb-5">
-                <div className="flex flex-col">
-                  <h3 className="text-3xl">{item.Role}</h3>
-                  {item.Position.map((extra) => {
-                    return (
-                      <div key={extra.fulltime} className="flex gap-5 mt-5">
-                        <p className="bg-white flex items-center gap-3 text-xl px-4 py-2">
-                          {extra.fulltime}
-                          <BiCheck className="h-7 w-7" />
-                        </p>
-                        <p className="bg-white flex items-center gap-3 text-xl px-4 py-2">
-                          {extra.remote} <BiCheck className="h-7 w-7" />
-                        </p>
-                      </div>
-                    );
-                  })}
+    <>
+      <Navbar />
+      <div className="max-w-6xl m-auto p-5">
+        <section className="relative w-full flex flex-row gap-5 justify-between">
+          <input
+            type="text"
+            className="border border[#d0d5dd] outline-none h-[50px] w-4/5 pl-8 rounded-lg"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search positions"
+          />
+          <AiOutlineSearch className="absolute top-7 left-2 w-7 h-7 text-[#b4b9bf]" />
+          <button
+            type="submit"
+            className="bg-main-1 border-none text-white flex justify-center items-center px-10 rounded-lg"
+          >
+            Search
+          </button>
+        </section>
+        <section className="mt-7 mb-10 flex gap-5 text-3xl">
+          <p onClick={() => setData(CareerPageData)} className="cursor-pointer">
+            All
+          </p>
+          <p
+            onClick={() => filterResult('Lead Generation')}
+            className="cursor-pointer"
+          >
+            Lead Generation
+          </p>
+          <p onClick={() => filterResult('Others')} className="cursor-pointer">
+            Others
+          </p>
+        </section>
+        {data
+          .filter((item) => {
+            return search.toLowerCase() === ''
+              ? item
+              : item.Role.toLowerCase().includes(search);
+          })
+          .map((item) => {
+            return (
+              <div key={item.sn}>
+                <h2 className="text-3xl">{item.Category}</h2>
+                <div className="bg-[#f0f0f0] flex justify-between items-center p-8 mt-8 mb-5">
+                  <div className="flex flex-col">
+                    <h3 className="text-3xl">{item.Role}</h3>
+                    {item.Position.map((extra) => {
+                      return (
+                        <div key={extra.fulltime} className="flex gap-5 mt-5">
+                          <p className="bg-white flex items-center gap-3 text-xl px-4 py-2">
+                            {extra.fulltime}
+                            <BiCheck className="h-7 w-7" />
+                          </p>
+                          <p className="bg-white flex items-center gap-3 text-xl px-4 py-2">
+                            {extra.remote} <BiCheck className="h-7 w-7" />
+                          </p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <a href={item.link}>
+                    <MdOutlineKeyboardArrowRight className="w-10 h-10" />
+                  </a>
                 </div>
-                <a href={item.link}>
-                  <MdOutlineKeyboardArrowRight className="w-10 h-10" />
-                </a>
               </div>
             );
           })}
