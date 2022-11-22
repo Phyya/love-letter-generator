932ddb415b6296a7d58c8422b0b7d4c8cfd991f4
Juicio <wisdomochiche@gmail.com>
2022-11-22 10:51:23 +0100

    Update Sitemap.jsx

--- src/pages/SiteMap/SiteMap.jsx
+++ src/pages/SiteMap/SiteMap.jsx
@@ -18,73 +18,73 @@ export default function SiteMap() {
         <section className="links-section">
           <div className="links--container">
             <p className="links--leading">Company</p>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/aboutus">
               About us
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/blog">
               Blog
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/careers">
               Careers
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/contactus">
               Contact us
             </Link>
           </div>
           <div className="links--container">
             <p className="links--leading">Product</p>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/login">
               Login
             </Link>
             <Link className="site-link" to="/">
               LoveMe Letter Generator
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/pricing">
               Pricing
             </Link>
           </div>
           <div className="links--container">
             <p className="links--leading">Resources</p>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/documentation">
               Documentation
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/api">
               API reference
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/sitemap">
               Sitemap
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/sdks">
               SDKs
             </Link>
           </div>
           <div className="links--container">
             <p className="links--leading">Support</p>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/helpcenter">
               Help Center
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/contactus">
               Contact Us
             </Link>
             <Link className="site-link" to="/">
               Report Link bug
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/faqs">
               FAQs
             </Link>
           </div>
           <div className="links--container">
             <p className="links--leading">Legals</p>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/privacypolicy">
               Privacy Policy
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/developerpolicy">
               Developer Policy
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/termsofservice">
               Terms & Conditions
             </Link>
-            <Link className="site-link" to="/">
+            <Link className="site-link" to="/cookiepolicy">
               Cookie Policy
             </Link>
           </div>
