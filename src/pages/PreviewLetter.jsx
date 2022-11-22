fda6ad37e704aec1de3e20cbadd7258a2f6d3e1a
Akpokighe Marvellous O <m.ovigwe@yahoo.com>
2022-11-22 16:03:59 +0100

    Feat(FRO-19): Preview letter - added

--- src/pages/PreviewLetter.jsx
+++ src/pages/PreviewLetter.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
-import { NewsLetterModal } from '../components/newsleeter/newslettermodal';
-import { ModalContent } from '../components/newsleeter/newslettercontent';
-
-const PreviewLetter = () => {
-  const [open, isOpen] = useState(true);
+import classes from '../styles/style.module.css';
+export const LetterForm = () => {
+  
 
   return (
     <>
-      <NewsLetterModal isOpen={isOpen} open={open}>
-        <ModalContent isOpen={isOpen} open={open} />
-      </NewsLetterModal>
-      <div>PreviewLetter</div>
+      <div className="mt-10 sm:mt-0">
+        <div className={`md:grid md:gap-6 ${classes.flexed_form_container}`}>
+          <div
+            className={`m-auto mt-5 md:col-span-2 md:mt-0 ${classes.boxedShadowed}`}
+          >
+              <div className={classes.hero_brand}>
+            <p className={classes.hero_brand_name}>Tell Me About Your Partner</p>
+
+           
+            <p className={classes.hero_brand_border}></p>
+          </div>
+            contents goes here
+          </div>
+        
+        </div>
+      </div>
     </>
   );
 };
 
-export default PreviewLetter;
+export default LetterForm
